59ac226a5b51f1c7d94d0e1e5b413a2d2cdf7c21
Erikson Gomes <eriksonlove@gmail.com>
2020-03-26 14:33:46 +0100

    Save

--- CVControl.Data/DataContext.cs
+++ CVControl.Data/DataContext.cs
@@ -24,36 +24,6 @@ namespace CVControl.Data
             modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
             modelBuilder.Conventions.Add(new StringConvention());
 
-            //modelBuilder.Entity<Rol>()
-            //    .HasMany(e => e.Utilizadores)
-            //    .WithMany(e => e.Roles)
-            //    .Map(m => m.ToTable("AspNetUserRoles").MapLeftKey("RoleId").MapRightKey("UserId"));
-
-            //modelBuilder.Entity<Utilizador>()
-            //    .HasMany(e => e.AspNetUserClaims)
-            //    .WithRequired(e => e.Utilizador)
-            //    .HasForeignKey(e => e.UserId);
-
-            //modelBuilder.Entity<Utilizador>()
-            //    .HasMany(e => e.AspNetUserLogins)
-            //    .WithRequired(e => e.Utilizador)
-            //    .HasForeignKey(e => e.UserId);
-
-            //modelBuilder.Entity<Utilizador>()
-            //    .HasMany(e => e.Permissoes)
-            //    .WithMany(e => e.Utilizadores)
-            //    .Map(m => m.ToTable("PermissoesUtilizador").MapLeftKey("IdUtilizador").MapRightKey("IdPermissao"));
-
-            //modelBuilder.Entity<Servico>()
-            //    .HasMany(e => e.SubCategorias)
-            //    .WithMany(e => e.Servicos)
-            //    .Map(m => m.ToTable("SubCategoriasServico").MapLeftKey("IdServico").MapRightKey("IdSubCategoria"));
-
-            //modelBuilder.Entity<Cliente>()
-            //    .HasMany(e => e.Favoritos)
-            //    .WithMany(e => e.Clientes)
-            //    .Map(m => m.ToTable("Favoritos").MapLeftKey("IdCliente").MapRightKey("IdServico"));
-
             modelBuilder.Entity<Resultado>()
                 .HasMany(e => e.Sintomas)
                 .WithMany(e => e.Resultados)
