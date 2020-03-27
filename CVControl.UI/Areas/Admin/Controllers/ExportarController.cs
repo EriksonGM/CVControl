02bd662ef1ea739fedc23b372a4d810b12211595
Erikson Gomes <eriksonlove@gmail.com>
2020-03-27 16:37:38 +0100

    Save

---
+++ CVControl.UI/Areas/Admin/Controllers/ExportarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVControl.UI.Areas.Admin.Controllers
+{
+    public class ExportarController : Controller
+    {
+        // GET: Admin/Exportar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
